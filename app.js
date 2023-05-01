ded1408b0eac6297dc960f848d1593ad92bb4192
skfeat <128950274+skfeat@users.noreply.github.com>
2023-05-01 16:23:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-const express = require("express");
+const User = require('./db');
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.json()); // Add this line
+app.use(cors());
 
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.get('/', async (req, res) => {
+  res.json({status:"ok",message:"Server is working fine"});
+});
+
+app.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Check if user already exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({message:'User already exists',status:false});
+  }
+  // Create a new user
+  const newUser = new User({ name, email, password });
+  try {
+    await newUser.save();
+    console.log('User created:', newUser.name);
+    res.json({message:"Account Created Successfully",data:newUser,status:true});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message:'Error creating user',status:false});
+  }
+});
+app.get(`/signup`,  (req, res) =>{
+res.json({error:"get method is not allowed"})
+})
+app.get(`/login`, (req, res) =>{
+  res.json({error:"get method is not allowed"})
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password });
+    if (user) {
+      res.json({message:"Logged in",data: user,status:true});
+    } else {
+      res.status(400).json({ message: 'Invalid User id or Password' ,status:false});
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong',status:false });
+  }
+});
+
+
+
+app.listen(3450, () => {
+  console.log('Server listening on port 3000');
+});
