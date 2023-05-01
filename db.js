4bf4f9f2dc0e193af7e4f37f97a11cf4ea0da6cf
skfeat <128950274+skfeat@users.noreply.github.com>
2023-05-01 16:22:28 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://bravebrowser1775:Raj%401775@cluster0.diayrzp.mongodb.net/UserData', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB Atlas');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB Atlas:', error);
+});
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
